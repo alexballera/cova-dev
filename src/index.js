db4cf3b5e11f710e91d25c092da14d65c16cf567
Alex Ballera <alexballera@gmail.com>
2017-07-06 21:46:55 -0400

    Configurando a node 7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 'use strict'
-global.jQuery = require('jquery')
-require('jquery-ui-dist/jquery-ui')
 import WebFont from './scripts/Webfont'
 import Layout from './components/layout'
 import topNavbar from './components/topNavbar'
 import Views from './views'
 import Footer from './components/footer'
 import {Navigation} from './components/navigation'
-(() => {
+global.jQuery = require('jquery')
+require('jquery-ui-dist/jquery-ui')(() => {
   document.addEventListener('DOMContentLoaded', onDOMLoad)
 
   function onDOMLoad () {
